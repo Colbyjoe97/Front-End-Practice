717936d5082765a70b7f0b2a675b17c556c30772
colbyjoe97 <dumonchelle123@gmail.com>
2022-02-07 16:11:21 -0500

    fixed animation

--- script.js
+++ script.js
@@ -1,52 +1,18 @@
-function play() {
-    anime({
-        targets: '.box',
-        translateY: [
-            {value: 200, duration: 600},
-            {value: 0, duration: 1000}
-        ],
-        rotate: {
-            value: '1turn',
-            easing: 'easeInOutSine'
-        },
-        delay: function(el, i, l){
-            return i * 1000
-        }
-    });
-    // anime({
-    //     targets: '.blue',
-    //     translateY: [
-    //         {value: 200, duration: 600, delay: 1000},
-    //         {value: 0, duration: 1000}
-    //     ],
-    //     rotate: {
-    //         value: '1turn',
-    //         easing: 'easeInOutSine',
-    //         delay: 1000,
-    //     }
-    //   });
-    // anime({
-    //     targets: '.green',
-    //     translateY: [
-    //         {value: 200, duration: 600, delay: 2000},
-    //         {value: 0, duration: 1000}
-    //     ],
-    //     rotate: {
-    //         value: '1turn',
-    //         easing: 'easeInOutSine',
-    //         delay: 2000,
-    //     }
-    //   });
-    // anime({
-    //     targets: '.yellow',
-    //     translateY: [
-    //         {value: 200, duration: 600, delay: 3000},
-    //         {value: 0, duration: 1000}
-    //     ],
-    //     rotate: {
-    //         value: '1turn',
-    //         easing: 'easeInOutSine',
-    //         delay: 3000,
-    //     }
-    //   });
-}
+var animation = anime({
+    targets: '.box',
+    translateY: [
+        {value: 200, duration: 600},
+        {value: 0, duration: 1000}
+    ],
+    rotate: {
+        value: '1turn',
+        easing: 'easeInOutSine'
+    },
+    delay: function(el, i, l){
+        return i * 1000
+    },
+    autoplay: false
+})
+
+document.querySelector('#boxes .pause').onclick = animation.pause;
+document.querySelector('#boxes .play').onclick = animation.play;
