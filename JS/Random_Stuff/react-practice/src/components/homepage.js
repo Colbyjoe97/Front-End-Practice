c7ad8af03ea4fcfcfed82a66bcc1dcfd1555609e
Colby Dumonchelle <dumonchelle123@gmail.com>
2022-07-05 13:57:22 -0400

    change background color

--- JS/Random_Stuff/react-practice/src/components/homepage.js
+++ JS/Random_Stuff/react-practice/src/components/homepage.js
@@ -7,6 +7,7 @@ export default class homepage extends Component {
 		this.state = {
 			count: 0,
 			show: false,
+			bg_color: ""
 		};
 	}
 
@@ -21,10 +22,24 @@ export default class homepage extends Component {
 		) : this.setState({show: true})
 	}
 
+	changeBg = (color) => {
+		console.log(color)
+		this.bg_color = color
+		let query = document.querySelector('.wrapper')
+		if(this.bg_color === "rgb(51, 50, 50)") {
+			query.style.backgroundColor = this.bg_color
+			query.style.color = "white";
+		}else {
+			query.style.backgroundColor = this.bg_color
+			query.style.color = "black";
+		}
+		console.log(query)
+	}
+
 	
 	render() {
 		return (
-			<div>
+			<div className="wrapper">
 				{/* INCREMENT NUMBER */}
 				<div className="increment">
 					<button onClick={this.increment}>Click Me!</button>
@@ -41,16 +56,26 @@ export default class homepage extends Component {
 						{this.state.show === false ? (
 							<span>
 								<br />
-								<span className="blue" onClick={this.showMore}>Show More..</span>
+								<span className="show" onClick={this.showMore}>Show More..</span>
 							</span>
 						) : (
 							<span>
 								<br />
 								Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptate ullam ipsum eveniet ea, voluptas atque minima impedit explicabo possimus quos. Voluptate nihil aliquid consequatur animi? <br />
-								<span className="blue" onClick={this.showMore}>Show Less..</span>
+								<span className="show" onClick={this.showMore}>Show Less..</span>
 							</span>
 						)}
 					</p>
+				</div>
+
+				<h1 className="text-center">Change Your Background Color!</h1>
+				<div className="colors">
+					<p className="circle red" onClick={ e => this.changeBg("red") }></p>
+					<p className="circle blue" onClick={ e => this.changeBg("blue") }></p>
+					<p className="circle green" onClick={ e => this.changeBg("green") }></p>
+					<p className="circle purple" onClick={ e => this.changeBg("purple") }></p>
+					<p className="circle dark" onClick={ e => this.changeBg("rgb(51, 50, 50)") }></p>
+					<p className="circle white" onClick={ e => this.changeBg("white") }></p>
 				</div>
 			</div>
 		);
